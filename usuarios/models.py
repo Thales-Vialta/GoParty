d0879337d5bef711aea1a6d27d7c5a2f58da0945
Thales-Vialta <thalesvialta12@gmail.com>
2025-11-07 10:32:57 -0300

    0.4 "feat: Added new model User"

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
-# Create your models here.
+class usuario(models.Model):
+    escolha_tipo = [('cliente','Cliente'),('fornecedor','Fornecedor'),('Admin','Administrador')] 
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    senha = models.CharField(max_length=200)
+    tipo = models.CharField(max_length=20, choices = escolha_tipo)
+    telefone = models.CharField(max_length=20, null=True)
+    
+    def __str__(self):
+        return self.nome
